e28e9ebc974a4738bba11ef5e509d7ea56218546
claranggraini <claraanggraini_01@yahoo.com>
2021-04-30 13:22:07 +0700

    add spinning image feature

--- Let It Spin/PlaySpinnerController.swift
+++ Let It Spin/PlaySpinnerController.swift
@@ -11,15 +11,61 @@ class PlaySpinnerController: UIViewController {
 
     @IBOutlet weak var boardIV: UIImageView!
     @IBOutlet weak var boardLbl: UILabel!
-    
     @IBOutlet weak var mainSpinnerIV: UIImageView!
-    
     @IBOutlet weak var spinBtn: UIButton!
+    
+    var startTime: CFTimeInterval?, endTime: CFTimeInterval?
+    
+    @IBAction func spinBtnOnClicked(_ sender: Any) {
+        spinBtn.alpha = 0.5
+        spinBtn.isEnabled = false
+        var displayLink = CADisplayLink(target: self, selector: #selector(stopSpin))
+        displayLink.add(to: .main, forMode: .common)
+        startTime = CACurrentMediaTime()
+        endTime = startTime! + 4
+        self.mainSpinnerIV.rotate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainSpinnerIV.image = UIImage(named: "Christmas Tree")
     }
 
+    @objc func stopSpin(displaylink: CADisplayLink){
 
+        guard
+            let endTime = endTime,
+            let startTime = startTime
+           else {
+             return
+         }
+
+        if CACurrentMediaTime() > endTime{
+            mainSpinnerIV.stopRotating()
+            displaylink.isPaused = true
+            displaylink.invalidate()
+            spinBtn.alpha = 1
+            spinBtn.isEnabled = true
+        }
+    }
 }
 
+extension UIImageView{
+    
+    func rotate() {
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = Double.random(in: 0.3..<0.4)
+        rotation.isCumulative = true
+        rotation.fillMode = .forwards
+        rotation.isRemovedOnCompletion = false
+        rotation.repeatCount = Float.greatestFiniteMagnitude
+        self.layer.add(rotation, forKey: "rotationAnimation")
+    }
+    
+    func stopRotating() {
+        let transform = self.layer.presentation()!.transform
+        self.layer.removeAllAnimations()
+        self.layer.transform = transform
+    }
+}
